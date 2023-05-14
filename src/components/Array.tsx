8bdc65987b4ceb11663b2cf98545ac00bb1b8c43
Glxy22 <nadiatx2015@gmail.com>
2023-05-14 01:38:18 -0500

    Nested Array Obj

--- src/components/Array.tsx
+++ src/components/Array.tsx
@@ -19,7 +19,23 @@ function Array() {
     const handleUpdate=()=> {
         setNames(Names.map( name => name == 'Alesha' ? 'Sara' : name));
     }
+//Nested Array
+    const [animals, setAnimal] = useState([
+        {
+            id : 1,
+            title:'Turtle',
+            Healthy:false
+        },
+        {
+            id:2,
+            title:'Mouse',
+            Healthy:false
+        }
+    ])
 
+    const handleAnimals= () => {
+        setAnimal(animals.map(animal => animal.id === 1 ? {...animal, Healthy: true } : animal))
+    }
   return (
     
     <div>
@@ -30,6 +46,13 @@ function Array() {
         <button onClick= {handleRemove}>Remove Name</button>
         {/* Update Name */}
         <button onClick= {handleUpdate}>Update Name</button>
+
+        
+        {/* Nested Array */}
+
+       <button onClick= {handleAnimals}>Update Animal Status</button>
+
+      {animals.map(animal =>  <p> {animal.id}{animal.title}{animal.Healthy ? 'Yes':'No'}</p>)}
 
     </div>
   )
